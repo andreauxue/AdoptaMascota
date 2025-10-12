1f99acf67258bdcca31921bf052a6acbf03c72b5
shucshin <robinucscs@gmail.com>
2025-10-11 19:58:02 -0600

    Del comment

--- Aplicacion/frontend/src/App.jsx
+++ Aplicacion/frontend/src/App.jsx
@@ -2,6 +2,5 @@ import Home from "./pages/Home";
 import Books from "./pages/Books";
 
 export default function App() {
-  // Muestra la galería de libros; si prefieres, cambia a <Home />
   return <Books />
 }
