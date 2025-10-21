afca13b1acb78c2a668cb531839f4d7f52507fd9
Alan Oliver López Robles <alanskull09@gmail.com>
2025-10-21 16:41:13 -0600

    Commit bien hecho

---
+++ Aplicacion/frontend/src/pages/Canciones.jsx
@@ -0,0 +1,9 @@
+// src/pages/Canciones.jsx
+import React from "react";
+import GaleriaCanciones from "../components/GaleriaCanciones";
+
+function Canciones() {
+  return <GaleriaCanciones />;
+}
+
+export default Canciones;
