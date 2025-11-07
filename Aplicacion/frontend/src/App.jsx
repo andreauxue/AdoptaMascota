f4fadca15c1a0718b0064aa6c220cfa0e2b02239
NerakCM <53309112+NerakCM@users.noreply.github.com>
2025-11-07 02:06:25 -0600

    Update App.jsx

--- Aplicacion/frontend/src/App.jsx
+++ Aplicacion/frontend/src/App.jsx
@@ -1,5 +1,26 @@
+// import { BrowserRouter, Router, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Galeria from "./pages/Galeria";
+
 
 export default function App() {
-  return <Home />
+  return (
+    <div className="min-h-screen bg-[#F7F7F7] flex flex-col">
+      <Navbar colorBg={"bg-blue-400"} />
+
+      {/* Contenedor que se expande */}
+      <main className="flex-1">
+        <Galeria />
+      </main>
+
+      <Footer
+        color={"bg-blue-400"}
+        texto={"En MatchPaw encontramos tu mascota perfecta"}
+      />
+    </div>
+  );
 }
+
