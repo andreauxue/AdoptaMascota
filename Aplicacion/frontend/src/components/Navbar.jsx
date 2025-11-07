041e1f055f4dc1d5cc6e874b47f38234f5f39807
BrunsO27 <bruno.ortiz_a10@ciencias.unam.mx>
2025-11-07 09:59:22 -0600

    chore: Actualización del Navbar y Footer

--- Aplicacion/frontend/src/components/Navbar.jsx
+++ Aplicacion/frontend/src/components/Navbar.jsx
@@ -1,24 +1,50 @@
-export default function Navbar({ colorBg, colorText = "text-white" }) {
-  return (
-    <nav
-      className={`${colorBg} ${colorText} flex justify-between items-center`}
-    >
-      <h1> MatchPaw </h1>
+import logo from '../assets/logo.png';
 
-      <ul className={`${colorText} flex gap-6`}>
+export default function Navbar() {
+  return (
+     <nav
+      className="w-full bg-[#38657E] text-white px-8 py-5 
+                 flex justify-between items-center shadow-md"
+    >
+      {/* Logo + Nombre */}
+      <div className="flex items-center gap-3">
+        <img 
+          src={logo} 
+          alt="MatchPaw logo"
+          className="w-10 h-10 object-contain"
+        />
+        <h1 className="text-2xl font-bold tracking-wide">MatchPaw</h1>
+      </div>
+
+      {/* Opciones del menú */}
+      <ul className="flex gap-8 text-lg font-medium">
         <li>
-          <a href="/" className="hover: underline">
+          <a href="/" className="hover:underline">
             Inicio
           </a>
         </li>
+
         <li>
-          <a href="/login" className="hover: underline">
-            Iniciar Sesion
+          <a href="/" className="hover:underline">
+            Ver Galería
           </a>
         </li>
+
         <li>
-          <a href="/register" className="hover: underline">
-            Registro
+          <a href="/galeria" className="hover:underline">
+            Búsqueda Filtrada
+          </a>
+        </li>
+
+        <li>
+          <a href="/registrar-mascota" className="hover:underline">
+            Registrar Mascota
+          </a>
+        </li>
+
+        <li>
+          <a href="/logout" className="hover:underline">
+            Cerrar Sesión
           </a>
         </li>
       </ul>
