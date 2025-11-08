4bce66cf0ce0d46d48270c9f10b980316cabdec8
Angel Moises Gonzalez Corrales <moises_gonzalez@ciencias.unam.mx>
2025-11-08 11:59:01 -0600

    feat(frontend): Se añadio el footer

--- Aplicacion/frontend/tailwind.config.js
+++ Aplicacion/frontend/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'azul-fondo': '#555B6E',
+        'verde-grisaseo': '#89B0AE',
+        'verde-menta': '#BEE3DB',
+        'durazno': '#FFD6BA',
+        'blanco' : '#FAF9F9'
+      },
+    },
   },
   plugins: [],
 }
