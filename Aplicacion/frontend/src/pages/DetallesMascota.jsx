000d8ca7e1d80877f435056087dde8ee6307e050
NerakCM <karencm@ciencias.unam.mx>
2025-11-22 21:06:48 -0600

    cambié el titulo

--- Aplicacion/frontend/src/pages/DetallesMascota.jsx
+++ Aplicacion/frontend/src/pages/DetallesMascota.jsx
@@ -13,7 +13,7 @@ export default function GalletaDetalles() {
 
       {/* Título */}
       <h1 className="text-3xl font-bold text-center text-black mt-2 mb-10">
-        Información Detallada
+        Perfil de Mascota
       </h1>
 
       {/* Contenedor principal */}
