6e53d9019e6cd20f366723867a2b25d5146a222b
ulisessap <ulisessap12@ciencias.unam.mx>
2025-11-24 09:33:10 -0600

    fix: logout en la navbar y cerrado de sesion correcto ahora si

--- Aplicacion/frontend/src/components/Navbar.jsx
+++ Aplicacion/frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
     FaPaw,
     FaHome,
@@ -13,10 +13,19 @@ import huellitasEnCasa from '../assets/huellitasEnCasa.jpg';
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
-    const { user } = useAuth(); 
+    //Carga de usuario actual y logout
+    const { user, logout } = useAuth(); 
+    //Para rederigor despues de hacer el logout
+    const navigate = useNavigate();
 
     const linkStyle = "flex items-center gap-2 px-4 py-2 rounded-md bg-[#10403B] hover:bg-[#243B55] transition-all duration-300 text-white font-medium cursor-pointer hover:shadow-[0_0_15px_rgba(255,255,255,0.3)] hover:ring-2 hover:ring-white/50 hover:-translate-y-1 hover:scale-105 active:scale-95 active:translate-y-0 font-belleza";
 
+    //Manejo de logout
+    const handleLogout = async () => {
+        await logout();
+        setIsOpen(false);
+        navigate("/");
+    };
     return (
         <nav className="bg-[#127369] text-white px-4 lg:px-8 py-4 flex flex-wrap justify-between items-center shadow-lg">
 
@@ -76,6 +85,18 @@ export default function Navbar() {
                                 </Link>
                             </li>
                         </>
+                    )}
+                    {/* si hay una sesion abierta se muestra el boton de logout */}
+                    {user && (
+                        <li>
+                            <button
+                                onClick={handleLogout}
+                                className={linkStyle}
+                            >
+                                <FaUser className="text-lg group-hover:scale-110 transition-transform" />
+                                <span className="font-medium">Cerrar Sesión</span>
+                            </button>
+                        </li>
                     )}
                 </ul>
             </div>
