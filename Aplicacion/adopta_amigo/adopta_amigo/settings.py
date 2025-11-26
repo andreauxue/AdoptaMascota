e8706ca9efbdd39bbb28d4514f5c855912ad34b0
andreauxue <andreauxue@ciencias.unam.mx>
2025-11-26 17:14:09 -0600

    fix: configuración

--- Aplicacion/adopta_amigo/adopta_amigo/settings.py
+++ Aplicacion/adopta_amigo/adopta_amigo/settings.py
@@ -64,8 +64,8 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://localhost:8005",
-    "http://127.0.0.1:8005",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
 ]
 CSRF_COOKIE_HTTPONLY = False
 
