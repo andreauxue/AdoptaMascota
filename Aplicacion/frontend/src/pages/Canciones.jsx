cd236f532253e0cd547c4871cac688638f367c29
Andrea Amaya <110924892+andreauxue@users.noreply.github.com>
2025-12-04 10:37:47 -0600

    Revert "Actualización Proyecto Final (alan-lopez)"

--- Aplicacion/frontend/src/pages/Canciones.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/Canciones.jsx
-import React from "react";
-import GaleriaCanciones from "../components/GaleriaCanciones";
-
-function Canciones() {
-  return <GaleriaCanciones />;
-}
-
-export default Canciones;
